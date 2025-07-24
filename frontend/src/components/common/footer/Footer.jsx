44a33ced8bbcf2a56b5e50a377461f4c8d36cfce
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-07-24 22:10:11 +0600

    Updating

--- frontend/src/components/common/footer/Footer.jsx
+++ frontend/src/components/common/footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      Footer section
+    <div className='bg-black text-white'>
+      Footer
     </div>
   )
 }
