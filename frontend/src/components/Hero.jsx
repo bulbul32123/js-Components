c16f95d58e5215910704c9ba7b7dd45863b34855
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-07-24 22:28:51 +0600

    Updating navbar

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <div className="flex justify-center items-center h-full w-full py-24">
       <div className="flex flex-col text-center w-full md:w-[700px]">
         <h1 className="text-[4rem] leading-[1] Smd:text-8xl font-bold">
-          Build your Own <span className='text-yellow font-bold bg-black leading-[1.5] px-3 rounded-md'>Components</span>
+          Build your Own <span className='text-yellow font-bold bg-black leading-[1.5] px-3 rounded-md'>Component</span>
         </h1>
         <p className="text-gray-500 max-md:px-7 -pt-5 leading-6 flex items-center gap-1 flex-wrap justify-center">
           A set of beautifully-designed, accessible components and an <Ai /> to Generate yours Component.
