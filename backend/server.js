fb5fd445f87b65a63f2b15b23ee0d9e3687275f8
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-07-26 23:10:07 +0600

    updates

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,14 @@ const { connectToMongoDB } = require('./db/connectToMongoDB')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
 connectToMongoDB();
 
-// Middleware
 app.use(express.json());
 
-// Route: /api/testing
 app.get('/api/testing', (req, res) => {
     res.json({ message: "Api is working" });
 });
 
-// Start Server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
